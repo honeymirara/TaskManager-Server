6e2f4806d0bcf4634640f68d656898477d3eb732
honeymirara <honeymirara@gmail.com>
2023-06-10 23:40:07 -0900

    add-new-controller

---
+++ src/controller/api.controller.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { buildResponse } = require('../helper/buildResponse');
+const route = express.Router();
+
+route.post('/', async (req, res) => {
+    try {
+        const { name, surname, email, pwd } = req.body;
+        const data = await createUser(name, surname, email, pwd);
+        buildResponse(res, 200, data);
+    } catch (err) {
+        buildResponse(res, 404, err.message);
+    }
+});
+
+module.exports = route;
